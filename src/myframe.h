d2f52d1a9bd1c0f4f9e6aed06acdd1f01f8959b7
Tobias Predel <tobias.predel@gmail.com>
2023-01-03 17:40:03 +0100

    Add filter and search icons to toolbar

--- src/myframe.h
+++ src/myframe.h
@@ -687,6 +687,9 @@ struct MyFrame : wxFrame {
             tb->AddControl(new wxStaticText(tb, wxID_ANY, _(L"Search ")));
             tb->AddControl(filter =
                 new wxTextCtrl(tb, A_SEARCH, "", wxDefaultPosition, wxSize(80, 22) * csf));
+            AddTBIcon(_(L"Go to Next Search Result"), A_SEARCHNEXT, iconpath + L"search.png");
+            AddTBIcon(_(L"Filter by Search Term"), A_FILTERS, iconpath + L"filter.png");
+            AddTBIcon(_(L"Filter off"), A_FILTEROFF, iconpath + L"filteroff.png");
             SEPARATOR;
             tb->AddControl(new wxStaticText(tb, wxID_ANY, _(L"Replace ")));
             tb->AddControl(replaces =
@@ -695,6 +698,8 @@ struct MyFrame : wxFrame {
             tb->AddControl(new wxStaticText(tb, wxID_ANY, _(L"Cell ")));
             celldd = new ColorDropdown(tb, A_CELLCOLOR, csf, 1);
             tb->AddControl(celldd);
+            AddTBIcon(_(L"Filter by cell background"), A_FILTERBYCELLBG, iconpath + L"filter.png");
+            AddTBIcon(_(L"Filter off"), A_FILTEROFF, iconpath + L"filteroff.png");
             SEPARATOR;
             tb->AddControl(new wxStaticText(tb, wxID_ANY, _(L"Text ")));
             textdd = new ColorDropdown(tb, A_TEXTCOLOR, csf, 2);
