7f811e9cceb17cf0a3ef4fb8fea60efab22a49f0
Tobias Predel <tobias.predel@gmail.com>
2023-02-22 18:34:33 +0100

    Darken non-matching cells in search

--- src/document.h
+++ src/document.h
@@ -1052,7 +1052,7 @@ struct Document {
             case A_CASESENSITIVESEARCH: {
                 sys->casesensitivesearch = !(sys->casesensitivesearch);
                 sys->cfg->Write(L"casesensitivesearch", sys->casesensitivesearch);
-                sys->frame->search(sys->searchstring);
+                this->Refresh();
                 return nullptr;
             }
 
