870f462cca9e6af967d43ff9adb3be060790d2b0
Tobias Predel <tobias.predel@gmail.com>
2023-04-06 21:49:04 +0200

    Jump to selection after first paint on Linux and Mac

--- src/mycanvas.h
+++ src/mycanvas.h
@@ -4,6 +4,7 @@ struct TSCanvas : public wxScrolledWindow {
 
     int mousewheelaccum;
     bool lastrmbwaswithctrl;
+    bool firstpaint;
 
     wxPoint lastmousepos;
 
@@ -13,7 +14,8 @@ struct TSCanvas : public wxScrolledWindow {
                            wxScrolledWindowStyle | wxWANTS_CHARS),
           mousewheelaccum(0),
           doc(nullptr),
-          lastrmbwaswithctrl(false) {
+          lastrmbwaswithctrl(false),
+          firstpaint(true) {
         SetBackgroundStyle(wxBG_STYLE_PAINT);
         SetBackgroundColour(*wxWHITE);
         DisableKeyboardScrolling();
@@ -38,6 +40,12 @@ struct TSCanvas : public wxScrolledWindow {
                    lastmousepos.y / doc->currentviewscale,
                    dc);
         */
+        #ifndef __WXMSW__
+        if (firstpaint) {
+            doc->ScrollIfSelectionOutOfView(dc, doc->selected);
+            firstpaint = false;
+        }
+        #endif
     };
 
     void UpdateHover(int mx, int my, wxDC &dc) {
