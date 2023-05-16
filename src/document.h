6b59e8e57df3b7698bbb20d7c2fd7060cb61db66
Tobias Predel <tobias.predel@gmail.com>
2023-05-16 14:50:02 +0200

    Copy HTML to clipboard
    Create a composite data object that is placed to the clipboard.
    
    The composite data object consists of
    - a custom data object (prefered) containing the HTML string and
    - a plain text data object (string).
    
    Please note that the HTML data is within a custom data object as
    a HTML data object keeps causing a buffer overflow (wxWidgets implementation)
    when one tries to paste from the clipboard.

--- src/document.h
+++ src/document.h
@@ -1279,14 +1279,20 @@ struct Document {
                     returnmessage = CopyImageToClipboard(c);
                 } else {
                     if (wxTheClipboard->Open()) {
-                        wxString s;
+                        wxString s, html;
                         if (k == A_COPYCT) {
                             loopallcellssel(c, true) if (c->text.t.Len()) s += c->text.t + " ";
                         } else {
                             s = selected.g->ConvertToText(selected, 0, A_EXPTEXT, this);
                         }
                         sys->clipboardcopy = s;
-                        wxTheClipboard->SetData(new wxTextDataObject(s));
+                        html = selected.g->ConvertToText(selected, 0, A_EXPHTMLT, this);
+                        wxDataObjectComposite *copyobj = new wxDataObjectComposite();
+                        wxCustomDataObject *htmlobj = new wxCustomDataObject(wxDF_HTML);
+                        htmlobj->SetData(html.Len(), html);
+                        copyobj->Add(new wxTextDataObject(s), false);
+                        copyobj->Add(htmlobj, true);
+                        wxTheClipboard->SetData(copyobj);
                         wxTheClipboard->Close();
                         returnmessage = _(L"Text copied to clipboard");
                     }
