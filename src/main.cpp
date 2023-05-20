274f010b8e055149f3a0c48a574060487ef0d3ec
Tobias Predel <tobias.predel@gmail.com>
2023-05-20 11:41:52 +0200

    Revert "Correct include order"
    This reverts commit 137ffbc369a71ee28ef429d4ec17f82b35f03590.

--- src/main.cpp
+++ src/main.cpp
@@ -290,11 +290,6 @@ struct treesheets {
 
     #include "treesheets_impl.h"
 
-    #include "mywxtools.h"
-    #include "mycanvas.h"
-    #include "myframe.h"
-    #include "myapp.h"
-
     #include "selection.h"
     #include "text.h"
     #include "cell.h"
@@ -303,6 +298,11 @@ struct treesheets {
 
     #include "document.h"
     #include "system.h"
+
+    #include "mywxtools.h"
+    #include "mycanvas.h"
+    #include "myframe.h"
+    #include "myapp.h"
 };
 
 treesheets::System *treesheets::sys = nullptr;
