07ead7ce93320b72903914494bbcb56cdb0dc618
Tobias Predel <tobias.predel@gmail.com>
2023-06-06 16:21:14 +0200

    Images need to rerendered when scale changes

--- src/myframe.h
+++ src/myframe.h
@@ -17,7 +17,7 @@ struct MyFrame : wxFrame {
     MyApp *app;
     wxFileSystemWatcher *watcher;
     bool watcherwaitingforuser;
-    double csf = FromDIP(1.0); // TODO: functions using this attribute should be modified to handle device-independent pixels
+    double csf; // TODO: functions using this attribute should be modified to handle device-independent pixels
     std::vector<std::string> scripts_in_menu;
     bool zenmode;
     ColorDropdown *celldd = nullptr;
@@ -92,6 +92,7 @@ struct MyFrame : wxFrame {
           fromclosebox(true),
           app(_app),
           watcherwaitingforuser(false),
+          csf(FromDIP(1.0)),
           watcher(nullptr),
           zenmode(false),
           searchmatchfound(false) {
@@ -1089,6 +1090,9 @@ struct MyFrame : wxFrame {
     }
 
     void OnDPIChanged(wxDPIChangedEvent &dce) {
+        csf = FromDIP(1.0);
+        loopv(i, sys->imagelist)
+            sys->imagelist[i]->bm_display = wxNullBitmap;
         if (nb) {
             loop(i, nb->GetPageCount()) {
                 TSCanvas *p = (TSCanvas *)nb->GetPage(i);
